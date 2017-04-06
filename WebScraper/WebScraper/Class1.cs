847d9fa97ebe034f27db47f5037fe90e6685a728
sreeder <stephanie.lyn.reeder@gmail.com>
2017-04-05 19:42:37 -0600

    add models and testing Moq objects

--- WebScraper/WebScraper/Class1.cs
+++ WebScraper/WebScraper/Class1.cs
@@ -8,5 +8,22 @@ namespace WebScraper
 {
     public class Class1
     {
+        public Dictionary<string, decimal> GetPrices(Product product)
+        {
+            return new Dictionary<string, decimal>();
+        }  
+        public string GetURL (Product product)
+        {
+            string baseURL = "https://www.pricecharting.com/game";
+            baseURL += product.Platform.Name.Replace(' ', '-');
+
+            return "";
+        }
+        //ebay
+        //amazon
+        //gamestop
+        //pricecharting
+
+
     }
 }
---
+++ WebScraper/WebScraperConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebScraperConsole")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c2e84eb-c0cb-481e-8052-e5bacb06b4fd")]
