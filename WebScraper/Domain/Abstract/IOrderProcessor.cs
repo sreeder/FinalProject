497e094d973844f66ee0a01a81960834ae7f7283
shadcarlson07 <shad.carlson@aggiemail.usu.edu>
2017-04-19 17:34:30 -0600

    deleting cart stuff

--- WebScraper/Domain/Abstract/IOrderProcessor.cs
+++ WebScraper/Domain/Abstract/IOrderProcessor.cs
@@ -9,6 +9,6 @@ namespace Domain.Abstract
 {
     public interface IOrderProcessor
     {
-        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+        void ProcessOrder( ShippingDetails shippingDetails);
     }
 }
