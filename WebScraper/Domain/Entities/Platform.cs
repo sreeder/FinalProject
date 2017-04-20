b2fbdf6ac45de8498eef06f3af5d905eec5173b9
sreeder <stephanie.lyn.reeder@gmail.com>
2017-04-19 18:05:55 -0600

    add new classes

---
+++ WebScraper/Domain/Entities/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Domain.Entities
+{
+    public class Platform
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PlatformID { get; set; }
+        [Required(ErrorMessage = "Please enter a product name")]
+        public string Name { get; set; }
+
+        public string Abbreviation { get; set; }
+       
+
+    }
+}
---
+++ WebScraper/Domain/Entities/Price.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Domain.Entities
+{
+    public class Price
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int ProductID { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        public decimal PriceNew { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        public decimal PriceUsed { get; set; }
+        
+     
+
+
+    }
+}
