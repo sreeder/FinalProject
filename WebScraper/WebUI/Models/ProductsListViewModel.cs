5b3599cceadb26424f246b3999402a20490a2e20
shadcarlson07 <shad.carlson@aggiemail.usu.edu>
2017-04-25 18:57:31 -0600

    adding json in model

--- WebScraper/WebUI/Models/ProductsListViewModel.cs
+++ WebScraper/WebUI/Models/ProductsListViewModel.cs
@@ -11,5 +11,49 @@ namespace WebUI.Models
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
         public string CurrentCategory { get; set; }
+        public string JsonProducts
+        {
+
+            get
+
+            {
+
+                var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+                var json = serializer.Serialize(Products.Select(p => p.Name));
+
+                var decodedJson = HttpUtility.HtmlDecode(json);
+
+
+
+                return json;
+
+            }
+
+        }
+
+        public int NumProducts
+
+        {
+
+            get
+
+            {
+
+                int count = 0;
+
+                foreach (Product p in Products)
+
+                {
+
+                    count++;
+
+                }
+
+                return count;
+
+            }
+
+        }
     }
 }
